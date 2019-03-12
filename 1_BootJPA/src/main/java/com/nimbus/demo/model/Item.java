aca25cffb61e9b02f05edc95784e5b43a110720a
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-12 07:33:08 -0400

    Adding ModelObject - Item.java

---
+++ 1_BootJPA/src/main/java/com/nimbus/demo/model/Item.java
@@ -0,0 +1,50 @@
+package com.nimbus.demo.model;
+
+public class Item {
+	
+	private int itemID;
+	private String itemName;
+	private String itemCompany;
+	private String itemCategory;
+	private long itemPrice;
+	
+	public int getItemID() {
+		return itemID;
+	}
+	public void setItemID(int itemID) {
+		this.itemID = itemID;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getItemCompany() {
+		return itemCompany;
+	}
+	public void setItemCompany(String itemCompany) {
+		this.itemCompany = itemCompany;
+	}
+	public String getItemCategory() {
+		return itemCategory;
+	}
+	public void setItemCategory(String itemCategory) {
+		this.itemCategory = itemCategory;
+	}
+	public long getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(long itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	
+	@Override
+	public String toString() {
+		return "Item [itemID=" + itemID + ", itemName=" + itemName + ", itemCompany=" + itemCompany + ", itemCategory="
+				+ itemCategory + ", itemPrice=" + itemPrice + "]";
+	}
+	
+	
+
+}
